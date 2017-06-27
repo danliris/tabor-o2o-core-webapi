7fc0e6a73df570a24b9b62f997e429793e5fcb2b
jordanmoonlay <jordan.leonardi@moonlay.com>
2017-06-27 12:46:14 +0700

    Tambah Remote Methods

--- common/models/order.js
+++ common/models/order.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Order) {
+   Order.remoteMethod('saveNewOrder', {
+    accepts: {arg: 'orderInstance', type: 'object'},
+    http: {path:'/Order_create', verb:'post'},
+    returns: {arg: 'result', type: 'object'}
+    });
 
+    Order.saveNewOrder = function(orderInstance, cb){
+    var order = new Order(orderInstance["order"])
+    order.save().then(function(savedOrder,err){
+    savedOrder.orderDetails.create(orderInstance["orderDetails"])
+    })
+}
 };
