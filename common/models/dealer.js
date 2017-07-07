c927aa5160bcd2cc36a87783a639d69bbef9f185
jordanmoonlay <jordan.leonardi@moonlay.com>
2017-07-07 10:28:52 +0700

    Modify API

--- common/models/dealer.js
+++ common/models/dealer.js
@@ -2,5 +2,5 @@
 
 module.exports = function(Dealer) {
 Dealer.validatesLengthOf('Code', { max:5, message: { max: 'Code should be 5- characters' } });
-Dealer.validatesUniquenessOf('Code',{message:'must unique'});
+Dealer.validatesUniquenessOf('Code',{message:'Code Is Already Taken'});
 };
