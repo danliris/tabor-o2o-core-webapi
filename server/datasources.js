617918287590e0588acaffc9fd3913e050a5c975
danliris <danliris.cloud@gmail.com>
2017-09-20 10:45:20 +0700

    Create datasources.js

---
+++ server/datasources.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "database": process.env.DB_NAME,
+    "password": process.env.DB_PASSWORD,
+    "user": process.env.DB_USER
+  }
+};
